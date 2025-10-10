7848c08c5f99e5ca121d7d7c95471dd86447c469
KoroYsabela <ysabelabathan2604@gmail.com>
2025-10-10 10:40:00 +0100

    setup: code ready for deployment to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
